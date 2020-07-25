f022a176c2c57d6066472a453ad3393e7c56008e
kumekanta <k.kanta.hokudai1104@gmail.com>
2020-07-26 04:13:05 +0900

    データ構造を変更

--- lib/components/add_memo_screen.dart
+++ lib/components/add_memo_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/rendering.dart';
 import 'memo_data.dart';
 // 表示用の書式設定
 const kLabelTextStyle = TextStyle(
@@ -17,20 +18,61 @@ class _AddMemoScreenState extends State<AddMemoScreen> {
   final myController = TextEditingController();
   // データ格納用リスト
   List<Map<String, dynamic>> items = [];
+  List<int> keyIds=[];
   // Keywordに入力された言葉を追加する
   void addKeyword(String inputkeyword) {
+    final prov=Provider.of<MemoData>(context, listen: false);
+    int loopcount = prov.keywordCount;
+    int keyId;
+    for(int i=0;i<loopcount;i++){
+      if(prov.keywordStore[i].keywordName== inputkeyword){
+        keyId = prov.keywordStore[i].keywordId;
+        keyIds.add(keyId);
+      }
+    }
     setState(() {
       _counter++;
-      items.add({"id": _counter, "title": inputkeyword});
+      items.add({"id": keyId, "title": inputkeyword});
     });
   }
   //　上記リストのカウント変数（ID用）
   int _counter = 0;
+  var _textFieldFocusNode;
+  var _inputController = TextEditingController();
+  var _chipList = List<Chip>();
+  var _keyNumber = 0;
+  @override
+  void initState() {
+    _textFieldFocusNode = FocusNode();
+    super.initState();
+  }
+  void _addChip(String text) {
+    var chipKey = Key('chip_key_$_keyNumber');
+    _keyNumber++;
+    _chipList.add(
+      Chip(
+        key: chipKey,
+        label: Text(text),
+        onDeleted: () => _deleteChip(chipKey),
+      ),
+    );
+  }
+  void _deleteChip(Key chipKey) {
+    setState(() => _chipList.removeWhere((Widget w) => w.key == chipKey));
+  }
   @override
   // widgetの破棄時にコントローラも破棄する
   void dispose() {
     myController.dispose();
+    _textFieldFocusNode.dispose();
     super.dispose();
+  }
+  void _onSubmitted(String text) {
+    setState(() {
+      _inputController.text = '';
+      _addChip(text);
+      FocusScope.of(context).requestFocus(_textFieldFocusNode);
+    });
   }
   @override
   Widget build(BuildContext context) {
@@ -108,64 +150,92 @@ class _AddMemoScreenState extends State<AddMemoScreen> {
               ],
             ), // Memo
             SizedBox(
-              height: 25,
+              height: 15,
+            ),
+//            Row(
+//              children: <Widget>[
+//                SizedBox(
+//                  width: 20,
+//                ),
+//                Expanded(
+//                  child: TextField(
+//                    autofocus: true,
+//                    textAlign: TextAlign.left,
+//                    decoration: const InputDecoration(
+//                      icon: Icon(Icons.link),
+//                      hintText: 'ADD  # hash tag !',
+//                    ),
+//                    controller: myController,
+////                    autofocus: true,
+////                    textAlign: TextAlign.center,
+////                    onChanged: (newMemoBody) {
+////                      _addingMemoBody = newMemoBody;
+////                    },
+//                  ),
+//                ),
+//                FlatButton(
+//                  child: Text(
+//                    '+',
+//                    style: TextStyle(
+//                      color: Colors.white,
+//                    ),
+//                  ),
+//                  color: Colors.lightBlueAccent,
+//                  onPressed: () {
+//                    addKeyword(myController.text);
+//                    // テキストフィールドの内容をクリア
+//                    myController.clear();
+//                  },
+//                ),
+//              ],
+//            ),
+//            SizedBox(
+//              height: 10,
+//            ),
+//            Expanded(
+//              child: ListView.builder(
+//                  scrollDirection: Axis.vertical,
+//                  shrinkWrap: true,
+//                  itemCount: items.length,
+//                  itemBuilder: (BuildContext context, int index) {
+//                    final item = items[index];
+//                    return new Card(
+//                      child: ListTile(
+////                        leading: Icon(Icons.link),
+//                        title:
+////                            Text(item["id"].toString() + ":  " + item["title"]),
+//                            Text("　＃  " + item["title"]),
+//                      ),
+//                    );
+//                  }),
+//            ),
+            TextField(
+              focusNode: _textFieldFocusNode,
+              autofocus: true,
+              controller: _inputController,
+              decoration: InputDecoration(
+                icon: Icon(Icons.link),
+                hintText: 'ADD  # hash tag !',
+              ),
+              onSubmitted: _onSubmitted,
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                SizedBox(
-                  width: 20,
-                ),
                 Expanded(
-                  child: TextField(
-                    autofocus: true,
-                    textAlign: TextAlign.left,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.link),
-                      hintText: 'ADD  # hash tag !',
-                    ),
-                    controller: myController,
-//                    autofocus: true,
-//                    textAlign: TextAlign.center,
-//                    onChanged: (newMemoBody) {
-//                      _addingMemoBody = newMemoBody;
-//                    },
+                  child: Wrap(
+                    alignment: WrapAlignment.start,
+                    spacing: 8.0,
+                    runSpacing: 0.0,
+                    direction: Axis.horizontal,
+                    children: _chipList,
                   ),
-                ),
-                FlatButton(
-                  child: Text(
-                    '+',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  color: Colors.lightBlueAccent,
-                  onPressed: () {
-                    addKeyword(myController.text);
-                    // テキストフィールドの内容をクリア
-                    myController.clear();
-                  },
                 ),
               ],
             ),
             SizedBox(
               height: 10,
-            ),
-            Expanded(
-              child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemCount: items.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final item = items[index];
-                    return new Card(
-                      child: ListTile(
-//                        leading: Icon(Icons.link),
-                        title:
-//                            Text(item["id"].toString() + ":  " + item["title"]),
-                        Text("　＃  " + item["title"]),
-                      ),
-                    );
-                  }),
             ),
             FlatButton(
               child: Text(
@@ -176,12 +246,15 @@ class _AddMemoScreenState extends State<AddMemoScreen> {
               ),
               color: Colors.lightBlueAccent,
               onPressed: () {
-                final prov = Provider.of<MemoData>(context, listen:false);
-                prov.addData(_addingMemoTitle, _addingMemoBody);
-                int id = prov.dataCount;
+                int _memoId =
+                    Provider.of<MemoData>(context, listen: false).memoId;
+                Provider.of<MemoData>(context, listen: false)
+                    .addData(_addingMemoTitle, _addingMemoBody, '', _memoId,keyIds);
+                Provider.of<MemoData>(context, listen: false).memoId++;
                 for(int i=0;i<items.length;i++){
-                  prov.registarKeyword(items[i]['title'], id);
+                  Provider.of<MemoData>(context, listen: false).registarKeyword(items[i]['title'], _memoId);
                 }
+                Navigator.pop(context);
               },
             ),
           ],
--- lib/components/memo_temp.dart
+++ lib/components/memo_temp.dart
@@ -5,8 +5,9 @@ class MemoTemp{
   String memoBody;
   String memoTitle;
   String photoName;
+  int memoId;
+  List<int> keywordsId;
 
-
-  MemoTemp({this.memoBody,this.memoTitle, this.photoName});
+  MemoTemp({this.memoBody,this.memoTitle, this.photoName, this.memoId, this.keywordsId});
 
 }
