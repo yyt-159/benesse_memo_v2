6092f4854884e58ab75604315ebbf5a2ca82c390
yyt159 <yyt159@yahoo.co.jp>
2020-07-26 04:39:53 +0900

    コンフリクト解消

--- lib/components/add_memo_screen.dart
+++ lib/components/add_memo_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/rendering.dart';
-import 'style.dart';
-
 import 'memo_data.dart';
 
 class AddMemoScreen extends StatefulWidget {
